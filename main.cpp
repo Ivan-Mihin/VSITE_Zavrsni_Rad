c7aaad2aee85e14ceb278de6dfca1f50726e2cf0
Ivan Mihin <mihin.ivan@outlook.com>
2024-09-08 17:37:06 +0200

    Create game loop

--- main.cpp
+++ main.cpp
@@ -1,22 +1,25 @@
+#include <iostream>
 #include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+using namespace std;
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+    RenderWindow window(VideoMode(800, 800), "TETRIS!", Style::Close);
 
     while (window.isOpen())
     {
         sf::Event event;
         while (window.pollEvent(event))
         {
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
         }
 
         window.clear();
-        window.draw(shape);
         window.display();
     }
 
