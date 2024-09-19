779f42d5e6efe399841df42803591c44d59cf7ec
Ivan Mihin <mihin.ivan@outlook.com>
2024-09-19 19:42:33 +0200

    Remove console

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
+#define NOMINMAX
+#include <windows.h>
 #include "game.h"
 
-int main()
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
     std::srand(static_cast<unsigned int>(std::time(nullptr)));
 
